67d5a0829860911583ceb897278e0547fc11b5ac
Seyd <34152761+SeydX@users.noreply.github.com>
2018-03-26 20:34:24 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var speedTest = require("speedtest-net"),
-	inherits = require("util").inherits,
-	moment = require('moment');
-	
+    inherits = require("util").inherits,
+    moment = require('moment');
+
 
 var Service,
     Characteristic,
@@ -10,10 +10,10 @@ var Service,
     HomebridgeAPI;
 
 module.exports = function(homebridge) {
-	
-	FakeGatoHistoryService = require('fakegato-history')(homebridge);
-	HomebridgeAPI = homebridge;
-	
+
+    FakeGatoHistoryService = require('fakegato-history')(homebridge);
+    HomebridgeAPI = homebridge;
+
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     Accessory = homebridge.platformAccessory;
@@ -23,22 +23,22 @@ module.exports = function(homebridge) {
 }
 
 function Broadband(log, config, api) {
-	
+
     var accessory = this;
 
     //HB
     this.config = config;
     this.log = log;
-    
+
     //BASE
     this.name = config["name"] || "Broadband";
     this.displayName = config.name;
-    this.maxTime = (config["maxTime"]*1000) || 5 * 1000;
-    this.interval = (config["interval"]*60*1000) || 60 * 60 * 1000;
+    this.maxTime = (config["maxTime"] * 1000) || 5 * 1000;
+    this.interval = (config["interval"] * 60 * 1000) || 60 * 60 * 1000;
     !this.dlspeed ? this.dlspeed = 0 : this.dlspeed;
     !this.ulspeed ? this.ulspeed = 0 : this.ulspeed;
     !this.ping ? this.ping = 0 : this.ping;
-    
+
     Characteristic.DownloadSpeed = function() {
         Characteristic.call(this, "Download", "da70da1f-da72-4db3-81c2-99f158a15a9a");
         this.setProps({
@@ -53,7 +53,7 @@ function Broadband(log, config, api) {
     };
     inherits(Characteristic.DownloadSpeed, Characteristic);
     Characteristic.DownloadSpeed.UUID = "da70da1f-da72-4db3-81c2-99f158a15a9a";
-    
+
     Characteristic.UploadSpeed = function() {
         Characteristic.call(this, "Upload", "ab74289e-d516-4a12-b2ae-1b32a74c035f");
         this.setProps({
@@ -68,7 +68,7 @@ function Broadband(log, config, api) {
     };
     inherits(Characteristic.UploadSpeed, Characteristic);
     Characteristic.UploadSpeed.UUID = "ab74289e-d516-4a12-b2ae-1b32a74c035f";
-    
+
     Characteristic.Ping = function() {
         Characteristic.call(this, "Ping", "cc65a09a-e052-410c-981d-c11bde2c3f60");
         this.setProps({
@@ -87,100 +87,102 @@ function Broadband(log, config, api) {
 }
 
 Broadband.prototype = {
-	
-  	getServices: function () {
-	  	
-	  	var self = this;
-	  	
-	    this.informationService = new Service.AccessoryInformation()
-	        .setCharacteristic(Characteristic.Name, this.name)
-	        .setCharacteristic(Characteristic.Identify, this.name)
-	        .setCharacteristic(Characteristic.Manufacturer, 'SeydX')
-	        .setCharacteristic(Characteristic.Model, 'Broadband Speedtest')
-	        .setCharacteristic(Characteristic.SerialNumber, "1234567890");
-	
-	    this.Sensor = new Service.TemperatureSensor(this.name);
-	    
-	    this.Sensor.getCharacteristic(Characteristic.CurrentTemperature)
-	        .setProps({
-	            minValue: 0,
-	            maxValue: 9999,
-	            minStep: 0.01
-	        })
-	        .updateValue(this.dlspeed);
-	        
+
+    getServices: function() {
+
+        var self = this;
+
+        this.informationService = new Service.AccessoryInformation()
+            .setCharacteristic(Characteristic.Name, this.name)
+            .setCharacteristic(Characteristic.Identify, this.name)
+            .setCharacteristic(Characteristic.Manufacturer, 'SeydX')
+            .setCharacteristic(Characteristic.Model, 'Broadband Speedtest')
+            .setCharacteristic(Characteristic.SerialNumber, "1234567890");
+
+        this.Sensor = new Service.TemperatureSensor(this.name);
+
+        this.Sensor.getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({
+                minValue: 0,
+                maxValue: 9999,
+                minStep: 0.01
+            })
+            .updateValue(this.dlspeed);
+
         this.Sensor.addCharacteristic(Characteristic.DownloadSpeed)
         this.Sensor.getCharacteristic(Characteristic.DownloadSpeed)
             .updateValue(this.dlspeed);
-            
+
         this.Sensor.addCharacteristic(Characteristic.UploadSpeed)
         this.Sensor.getCharacteristic(Characteristic.UploadSpeed)
             .updateValue(this.ulspeed);
-            
+
         this.Sensor.addCharacteristic(Characteristic.Ping)
         this.Sensor.getCharacteristic(Characteristic.Ping)
             .updateValue(this.ping);
-            
+
         this.historyService = new FakeGatoHistoryService("weather", this, {
             storage: 'fs',
             disableTimer: true,
             path: HomebridgeAPI.user.cachedAccessoryPath()
         });
-	        
-	    this.getData();    
-	  	
-	  	return [this.informationService, this.Sensor, this.historyService];
-	  	
-	},
-	
-	getData: function() {
-		
-		var self = this;
-		
-		speedTest({maxTime: self.maxTime}) 
-			.on('data', data => {
-				self.dlspeed = data.speeds.download;
-				self.ulspeed = data.speeds.upload;
-				self.ping = data.server.ping;
-				
-				self.log("Download: " + self.dlspeed + " Mbps");
-				self.log("Upload: " + self.ulspeed + " Mbps");
-				self.log("Ping: " + self.ping + " ms");
-			  
-		        self.historyService.addEntry({
-		            time: moment().unix(),
-		            temp: self.dlspeed,
-		            pressure: self.ping,
-		            humidity: self.ulspeed
-		        });
-			  
-				self.Sensor.getCharacteristic(Characteristic.CurrentTemperature).updateValue(self.dlspeed);
-				self.Sensor.getCharacteristic(Characteristic.DownloadSpeed).updateValue(self.dlspeed);
-				self.Sensor.getCharacteristic(Characteristic.UploadSpeed).updateValue(self.ulspeed);
-				self.Sensor.getCharacteristic(Characteristic.Ping).updateValue(self.ping);
-				setTimeout(function(){
-					self.getData();
-			  	}, self.interval)
-			})
-			.on('error', err => {
-				self.log("An error occured: " + err + " - Trying again in 1 min");
-				self.dlspeed = self.dlspeed;
-				self.ulspeed = self.ulspeed;
-				self.ping = self.ping;
-				self.Sensor.getCharacteristic(Characteristic.CurrentTemperature).updateValue(self.dlspeed);
-				self.Sensor.getCharacteristic(Characteristic.DownloadSpeed).updateValue(self.dlspeed);
-				self.Sensor.getCharacteristic(Characteristic.UploadSpeed).updateValue(self.ulspeed);
-				self.Sensor.getCharacteristic(Characteristic.Ping).updateValue(self.ping);
+
+        this.getData();
+
+        return [this.informationService, this.Sensor, this.historyService];
+
+    },
+
+    getData: function() {
+
+        var self = this;
+
+        speedTest({
+                maxTime: self.maxTime
+            })
+            .on('data', data => {
+                self.dlspeed = data.speeds.download;
+                self.ulspeed = data.speeds.upload;
+                self.ping = data.server.ping;
+
+                self.log("Download: " + self.dlspeed + " Mbps");
+                self.log("Upload: " + self.ulspeed + " Mbps");
+                self.log("Ping: " + self.ping + " ms");
+
+                self.historyService.addEntry({
+                    time: moment().unix(),
+                    temp: self.dlspeed,
+                    pressure: self.ping,
+                    humidity: self.ulspeed
+                });
+
+                self.Sensor.getCharacteristic(Characteristic.CurrentTemperature).updateValue(self.dlspeed);
+                self.Sensor.getCharacteristic(Characteristic.DownloadSpeed).updateValue(self.dlspeed);
+                self.Sensor.getCharacteristic(Characteristic.UploadSpeed).updateValue(self.ulspeed);
+                self.Sensor.getCharacteristic(Characteristic.Ping).updateValue(self.ping);
+                setTimeout(function() {
+                    self.getData();
+                }, self.interval)
+            })
+            .on('error', err => {
+                self.log("An error occured: " + err + " - Trying again in 1 min");
+                self.dlspeed = self.dlspeed;
+                self.ulspeed = self.ulspeed;
+                self.ping = self.ping;
+                self.Sensor.getCharacteristic(Characteristic.CurrentTemperature).updateValue(self.dlspeed);
+                self.Sensor.getCharacteristic(Characteristic.DownloadSpeed).updateValue(self.dlspeed);
+                self.Sensor.getCharacteristic(Characteristic.UploadSpeed).updateValue(self.ulspeed);
+                self.Sensor.getCharacteristic(Characteristic.Ping).updateValue(self.ping);
                 setTimeout(function() {
                     self.getData();
                 }, 60000)
-			});
-		
-	},
-	
-	identify: function (callback) {
-		this.log(this.name + ": Identified!");
-		callback();
-  	}
-	
+            });
+
+    },
+
+    identify: function(callback) {
+        this.log(this.name + ": Identified!");
+        callback();
+    }
+
 }
